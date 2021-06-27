645081bf0bfa10429318e4f35f80c4a6af406eae
selvaCareti <selva.jaya@careti.com>
2021-06-27 18:21:42 +0530

    first commit

---
+++ precache-manifest.df424237150e945992f5e00acce0eb60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c356f679b4e91606586da692d2a3f997",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be94776def97ab512224",
+    "url": "./static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "cefddc7c790f06c85ef8",
+    "url": "./static/css/main.0d03612f.chunk.css"
+  },
+  {
+    "revision": "be94776def97ab512224",
+    "url": "./static/js/2.68ef066e.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "./static/js/2.68ef066e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefddc7c790f06c85ef8",
+    "url": "./static/js/main.73c16dbb.chunk.js"
+  },
+  {
+    "revision": "2f9d6085a951d809a884",
+    "url": "./static/js/runtime-main.94f3020a.js"
+  },
+  {
+    "revision": "a80cdb8124d62c079ca8cd17b5f398cd",
+    "url": "./static/media/Luxury+Collection+Desktop.a80cdb81.jpg"
+  },
+  {
+    "revision": "0cb786edccd69390e042ac4ae53bec24",
+    "url": "./static/media/bg.0cb786ed.jpg"
+  }
+]);
